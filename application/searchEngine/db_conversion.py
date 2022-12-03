e247fc080ea1412cf99720ea06ca94bbccefeaee
Danh Nguyen <danhn2@illinois.edu>
2022-12-02 19:32:36 -0600

    search engine revamp - now can take in any string, not just movie title

--- application/searchEngine/db_conversion.py
+++ application/searchEngine/db_conversion.py
@@ -8,13 +8,30 @@ data = json.load(f)
 if os.path.isfile(os.getcwd() + '\movies.dat'):
     os.remove(os.getcwd() + '\movies.dat')
 
+if os.path.isfile(os.getcwd() + '\movies_whole.dat'):
+    os.remove(os.getcwd() + '\movies_whole.dat')
+
 if os.path.isfile(os.getcwd() + '\movies_ids.dat'):
     os.remove(os.getcwd() + '\movies_ids.dat')
 
 with open('movies.dat', 'a') as write_file:
     for id in data:
         # print(id, data[id])
-        write_file.write(data[id]['localized_title'].strip().lower() +'\n')
+        write_file.write(data[id]['localized_title'].strip().lower() + '\n')
+
+with open('movies_whole.dat', 'a', encoding='utf-8') as write_file:
+    for id in data:
+        info = data[id]['localized_title'].strip().lower()
+        for c in data[id]['cast']:
+            info += ' ' + c.strip().lower()
+        for g in data[id]['genres']:
+            info += ' ' + g.strip().lower()
+        for p in data[id]['producer']:
+            info += ' ' + p.strip().lower()
+        for d in data[id]['director']:
+            info += ' ' + d.strip().lower()
+        info += ' ' + str(data[id]['year'])
+        write_file.write(info + '\n')
 
 with open('movies_ids.dat', 'a') as write_file:
     for id in data:
@@ -22,6 +39,13 @@ with open('movies_ids.dat', 'a') as write_file:
 
 source = os.getcwd() + '\movies.dat'
 destination = os.getcwd() + '\movies\movies.dat'
+try:
+    shutil.move(source,destination)
+except FileNotFoundError:
+    print(source + " was not found")
+
+source = os.getcwd() + '\movies_whole.dat'
+destination = os.getcwd() + '\movies_whole\movies_whole.dat'
 try:
     shutil.move(source,destination)
 except FileNotFoundError:
